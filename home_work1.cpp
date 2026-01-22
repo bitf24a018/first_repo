ba01783c958e451a4cf6d9d9b7032f87f9e31cdc
bitf24a018 <bitf24a018@pucit.edu.pk>
2026-01-22 14:02:25 +0500

    modifiying mistake

--- home_work1.cpp
+++ home_work1.cpp
@@ -6,36 +6,37 @@ int *arr;
 int maxSize,currSize;
 public:
 unsorted(int n){
+    if(n>0){
     maxSize=n;
     arr=new int[n];
-    currSize=0;}
+    currSize=0;
+}
+}
 
     unsorted(const unsorted &other){
-        if(arr!=other.arr){
             maxSize=other.maxSize;
             arr=new int[maxSize];
             currSize=other.currSize;
-            for(int i=0;i<maxSize;i++){
+            for(int i=0;i<currSize;i++){
                 arr[i]=other.arr[i];
-            }
         }
 }
 
-unsorted operator=(const unsorted &other){
-    if(arr!=other.arr){
+unsorted& operator=(const unsorted &other){
+    if(this!=&other){
         delete[]arr;
         maxSize=other.maxSize;
         currSize=other.currSize;
-          for(int i=0;i<maxSize;i++){
+            arr=new int[maxSize];
+          for(int i=0;i<currSize;i++){
                 arr[i]=other.arr[i];
             }
     }
     return *this;
 }
 ~unsorted(){
-    if (arr!=nullptr){
+
         delete []arr;
         arr=nullptr;
-    }
 }
 };
